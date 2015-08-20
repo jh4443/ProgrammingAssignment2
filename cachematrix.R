90da40a0d2ee052a68cf99cc1c1f2b424cf05c16
HE <jh4443@nyu.edu>
2015-08-20 01:04:29 -0400

    homework two

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,14 @@
 
 # makeCacheMatrix contains four functions: set, setinverse, get, getinverse
 makeCacheMatrix <- function(x = matrix()) {
-        inverse_of_M  <- NULL 
+        inverse_the_mat  <- NULL
         set = function(y){                                                            # set matrix
                 x <<- y
-                inverse_of_M <<- NULL
+                inverse_the_mat <<- NULL
         }
         get <- function() x                                                           # get matrix                           
-        setinverse <- function(inverse) inverse_of_M <- inverse                       # set inverse of matrix
-        getinverse <- function() inverse_of_M                                         # get inverse of matrix              
+        setinverse <- function(inverse) inverse_the_mat <- inverse                    # set inverse of matrix
+        getinverse <- function() inverse_the_mat                                      # get inverse of matrix
         list(set = set, get = get, setinverse = setinverse,getinverse = getinverse)
 }
 
@@ -18,13 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
 # cacheSolve function calculate the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        inverse_of_M <- x$getinverse()                # Know the property of cache
-        if(!is.null(inverse_of_M)) {                  # check the cache data whether it is empty
+        inverse_the_mat <- x$getinverse()                         # Know the property of cache
+        if(!is.null(inverse_the_mat)) {                           # check the cache data whether it is empty
                 message("getting cached data")
-                return(inverse_of_M)                  # If it isn't empty, return the cache
+                return(inv)                                       # If it isn't empty, return the cache
         }
-        mat <- x$get()                                # get the matrix
-        inverse_of_M <- solve(mat)                    # calculate the inverse matrix
-        x$setinverse(inverse_of_M)                    # set the inverse matrix
-        inverse_of_M                                  # output the inverse matrix
+        mat <- x$get()                                            # get the matrix
+        inverse_the_mat <- solve(mat)                             # calculate the inverse matrix
+        x$setinverse(inverse_the_mat)                             # set the inverse matrix
+        inverse_the_mat                                           # output the inverse matrix
 }
